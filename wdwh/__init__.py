ae84f8bef46e67232e6cc0e01e14005de77f0da5
Kentaro Yasuda <93289950+ykentaro@users.noreply.github.com>
2022-10-31 23:10:41 -0400

    Added Login and Register
    Using users.json to store usernames and encrypted passwords to login and register.

--- wdwh/__init__.py
+++ wdwh/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+# from flask_bcrypt import Bcrypt
+import bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "e030a8c3cbfc01c4a941a37f0d527d19"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+
+cryp_salt = bcrypt.gensalt()
+# bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
 from wdwh import routes
--- wdwh/forms.py
+++ wdwh/forms.py
@@ -12,10 +12,10 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
     
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError("That username is taken. Please choose a different one.")
+    # def validate_username(self, username):
+    #     user = User.query.filter_by(username=username.data).first()
+    #     if user:
+    #         raise ValidationError("That username is taken. Please choose a different one.")
 
 class LoginForm(FlaskForm):
     username = StringField('Username',
