4c7d30cb8d557dbfae84417d9156d59c18d60423
vyasj <69089217+vyasj@users.noreply.github.com>
2022-11-01 02:33:45 -0400

    Changed db structure
    json database structure changed and added login checking

--- wdwh/routes.py
+++ wdwh/routes.py
@@ -1,8 +1,8 @@
 import sqlite3
 from sqlite3 import Error
-from wdwh import app, db, bcrypt
+from wdwh import app, db, bcrypt, db_functions
 from wdwh.forms import *
-from wdwh.models import User, Ingredient, Pantry, Recipe
+from wdwh.models import User, load_user, load_user_from_username
 from flask import Flask, render_template, flash, redirect, url_for
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -27,14 +27,13 @@ def register():
         return redirect(url_for("home"))
     register_form = RegistrationForm()
     if register_form.validate_on_submit():
-        # Add account information to database
-        hashed_pw = bcrypt.generate_password_hash(register_form.password.data).decode("utf-8")
-        user = User(username=register_form.username.data, password=hashed_pw)
-        db.session.add(user)
-        db.session.commit()
-        flash(f"Account created! Please login.","success")
-        return redirect(url_for("login"))
-    return render_template("register.html", title="Register", form=register_form)
+        if db_functions.create_user(register_form.username.data, register_form.password.data):
+            flash(f"Account created! Please login.","success")
+            return redirect(url_for("login"))
+        else:
+            flash("Username already taken, please pick another one", "danger")
+    return render_template("register.html", title="register", form=register_form)
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -42,10 +41,8 @@ def login():
         return redirect(url_for("home"))
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        # Check database if username/password combo are valid
-        user = User.query.filter_by(username=login_form.username.data).first()
-        if user and bcrypt.check_password_hash(user.password, login_form.password.data):
-            login_user(user, remember=login_form.remember.data)
+        if db_functions.validate_login(login_form.username.data, login_form.password.data):
+            login_user(load_user_from_username(login_form.username.data), remember=login_form.remember.data)
             flash(f"Welcome back!")
             return redirect(url_for("home"))
         else:
