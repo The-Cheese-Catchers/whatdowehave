00c72ced9036af6220a5a276c6afaf2b8f962f53
Jaimin Vyas <vyasj@github.com>
2022-11-02 02:08:36 -0400

    commit

--- wdwh/db_functions.py
+++ wdwh/db_functions.py
@@ -7,17 +7,17 @@ DB_PATH = "wdwh/data.json"
 
 def load_data():
     """
-        Loads the database and returns the data in a dictionary
+    Loads the database and returns the data in a dictionary
     """
-    data_file = open(DB_PATH, 'r')
-    json_data_file = json.load(data_file)
-    data_file.close()
+    with open(DB_PATH, 'r') as data_file:
+        json_data_file = json.load(data_file)
+        
     return json_data_file
 
 
-def create_user(username, password):
+def create_user(username: str, password: str) -> bool:
     """
-        Inserts a new user into the database with the given username/password combination
+    Inserts a new user into the database with the given username/password combination
     """
     data = load_data()
     
@@ -25,23 +25,23 @@ def create_user(username, password):
         return False
     
     hashed_password = bcrypt.hashpw(password.encode(), cryp_salt).decode()
-    data[username] = {
+    user_data = {
         "id" : len(data) + 1,
         "password" : hashed_password,
         "pantry" : {}
     }
+    data[username] = user_data
     
-    data_file = open(DB_PATH, 'w+')
-    json_dumpfile = json.dumps(data, indent=4)
-    data_file.write(json_dumpfile)
-    data_file.close()
+    with open(DB_PATH, 'w+') as data_file:
+        json_dumpfile = json.dumps(data, indent=4)
+        data_file.write(json_dumpfile)
     
     return True
 
 
-def validate_login(username, password):
+def validate_login(username: str, password: str) -> bool:
     """
-        Validates the login request by checking if the given username/password combination is in the database
+    Validates the login request by checking if the given username/password combination is in the database
     """
     data = load_data()
     
--- wdwh/forms.py
+++ wdwh/forms.py
@@ -4,34 +4,41 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from wdwh.models import User
 
+
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', 
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField('Password', 
-                             validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-    
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Sign Up")
+
     # def validate_username(self, username):
     #     user = User.query.filter_by(username=username.data).first()
     #     if user:
     #         raise ValidationError("That username is taken. Please choose a different one.")
 
+
 class LoginForm(FlaskForm):
-    username = StringField('Username',
-                        validators=[DataRequired()])
-    password = PasswordField('Password', 
-                             validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
+
 
 class EnterRecipeForm(FlaskForm):
-    recipe_name = StringField('Recipe Name',
-                              validators=[DataRequired()])
-    ingredients = TextAreaField('Enter ingredients, separated by a comma (,)',
-                                validators=[DataRequired()])
-    instructions = TextAreaField('Write some instructions on how to make this recipe',
-                                 validators=[DataRequired()])
-    picture = FileField('Upload a picture of the finished product', validators=[FileAllowed(['jpg','png'])])
-    submit = SubmitField('Make Recipe Public')
+    recipe_name = StringField("Recipe Name", validators=[DataRequired()])
+    ingredients = TextAreaField(
+        "Enter ingredients, separated by a comma (,)", validators=[DataRequired()]
+    )
+    instructions = TextAreaField(
+        "Write some instructions on how to make this recipe",
+        validators=[DataRequired()],
+    )
+    picture = FileField(
+        "Upload a picture of the finished product",
+        validators=[FileAllowed(["jpg", "png"])],
+    )
+    submit = SubmitField("Make Recipe Public")
