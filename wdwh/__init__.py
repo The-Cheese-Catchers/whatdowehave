a29ec7ecffe72d53844217d1c70c8ce499d92aff
rupakstephen <rupak.stephen@gmail.com>
2022-12-04 11:41:34 -0500

    added in env vars

--- wdwh/__init__.py
+++ wdwh/__init__.py
@@ -6,21 +6,19 @@ import os
 
 app = Flask(__name__)
 
-PASSWORD = os.getenv("APP_PASSWORD")
-PUBLIC_IP_ADDRESS = os.getenv("PUBLIC_IP_ADDRESS")
-DBNAME = os.getenv("DBNAME")
-PROJECT_ID = os.getenv("PROJECT_ID")
-INSTANCE_NAME = os.getenv("INSTANCE_NAME")
+DBCONNSTRING = os.getenv("DB_CONN")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ENV_TYPE = os.getenv("ENV_TYPE")
 
-ENV = "prod"
+ENV = ENV_TYPE
 
 
-app.config["SECRET_KEY"] = "b'c\xd4T=@\xfckz\xdc\x1eP\xcc\x84\xd4\xadfC\x8fq$\xbb\x82\xab\x19'"
+app.config["SECRET_KEY"] = SECRET_KEY
 #app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
-if ENV == 'dev':
-    app.config["SQLALCHEMY_DATABASE_URI"]= "postgresql://postgres:root@localhost/test"
+if ENV_TYPE == 'prod':
+    app.config["SQLALCHEMY_DATABASE_URI"]= DBCONNSTRING
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://ilqndltcvfcqgl:3b4300eea5c8e0692c301aa634a39cca95b750cb54d330b5f3c8ecff49f91304@ec2-44-195-132-31.compute-1.amazonaws.com:5432/d6tk7fgd8okbi4"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:root@localhost/test"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
 
 #since we are running one application, we do not need to do any thing fancy, just pass app into SQLAlchemy
